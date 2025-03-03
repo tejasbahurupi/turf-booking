d0be86fcb93a678d8258b07cbbfebbe87d6b3869
tejasbahurupi <tjbahurupi07@gmail.com>
2025-03-03 19:28:04 +0530

    deployment modification

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import adminRoute from './routes/adminRoutes.js';
-import userRoute from './routes/userRoutes.js';
-import connectdb from './dbconfig/connectdb.js';
+import express from "express";
+import cors from "cors";
+import adminRoute from "./routes/adminRoutes.js";
+import userRoute from "./routes/userRoutes.js";
+import connectdb from "./dbconfig/connectdb.js";
 
 const app = express();
 
@@ -10,20 +10,28 @@ const app = express();
 connectdb();
 
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://turf-booking-user.vercel.app/",
+      "https://turf-booking-admin.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 //routes
-app.use('/api/v1/admin', adminRoute);
-app.use('/api/v1/user', userRoute);
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/user", userRoute);
 
 //img uploads
-app.use('uploads/', express.static('uploads'));
+app.use("uploads/", express.static("uploads"));
 
 //port
 const PORT = process.env.PORT || 8080;
 
-//listen 
+//listen
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on port: ${PORT}`);
 });
--- server/models/groundModel.js
+++ server/models/groundModel.js
@@ -2,40 +2,39 @@ import mongoose from "mongoose";
 import { bookingSchema } from "./bookingModel.js";
 
 const groundSchema = new mongoose.Schema({
-    ground_name: {
-        type: String,
-        required: [true, "Name is required"],
+  ground_name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  location: {
+    type: String,
+    required: [true, "location is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "description is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "price is required"],
+  },
+  published: {
+    type: Boolean,
+    required: [true, "required"],
+  },
+  coordinates: {
+    latitude: {
+      type: String,
+      required: [true, "required"],
     },
-    location: {
-        type: String,
-        required: [true, "location is required"],
-
+    longitude: {
+      type: String,
+      required: [true, "required"],
     },
-    description: {
-        type: String,
-        required: [true, "description is required"],
-    },
-    price: {
-        type: Number,
-        required: [true, "price is required"],
-    },
-    published: {
-        type: Boolean,
-        required: [true, "required"],
-    },
-    coordinates: {
-        latitude: {
-            type: String,
-            required: [true, "required"],
-        },
-        longitude: {
-            type: String,
-            required: [true, "required"],
-        },
-    },
-    availableSlots: [String],
-    images: [String],
-    bookings: [bookingSchema]
+  },
+  availableSlots: [String],
+  images: [String],
+  bookings: [bookingSchema],
 });
 
-export const Ground = mongoose.model('Ground', groundSchema);
+export const Ground = mongoose.model("Ground", groundSchema);
